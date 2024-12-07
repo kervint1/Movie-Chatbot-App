343db2c704547c68cfa07dd7272df2be493119c0
kervint1 <s1f102302124@iniad.org>
2024-12-07 10:46:16 +0900

    ファイル構成、データベース設計

--- movie_management/views.py
+++ movie_management/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MovieForm
+from .models import Movie  # Movie モデルをインポート
+
+def add_movie(request):
+    if request.method == "POST":
+        form = MovieForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # ホームページにリダイレクト
+    else:
+        form = MovieForm()
+    return render(request, 'add_movie.html', {'form': form})
 
 def home(request):
-    return render(request, 'home.html')  # ホームページ
-
-def chatbot(request):
-    return render(request, 'chatbot.html')  # チャットボットページ
+    movies = Movie.objects.all()  # 登録済み映画を取得
+    return render(request, 'home.html', {'movies': movies})
